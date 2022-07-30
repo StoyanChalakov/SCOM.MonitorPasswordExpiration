f3591213ddfce1d76187cb4eae2fc21fa4abacc2
Stoyan Chalakov <stch006@hotmail.com>
2022-07-30 05:53:07 +0200

    fixed  use case, when no passwords are expiring

--- MonitorPasswordExpiration.ps1
+++ MonitorPasswordExpiration.ps1
@@ -4,12 +4,12 @@
     Script to report expiring user account passwords. Used within a PowerShell based SCOM monitor.
 .DESCRIPTION
     The script queries a specific group, gets the password expiration date for each user and copares it to predefined threshold of  20 days.
-    If the account password is about to expire in the comming 20 days a warning will be raised. 
+    If the account password is about to expire in the comming 20 days a warning will be raised.
 .NOTES
     Adjustable parameters are the $ADGroup to be queried and the $WarningDate timespan.
 #>
 
-    
+
 # Any Arguments specified will be sent to the script as a single string.
 # If you need to send multiple values, delimit them with a space, semicolon or other separator and then use split.
 
@@ -30,20 +30,20 @@ $Error.Clear()
 #Prepare API and Property Bag Objects
 $ScomAPI = New-Object -ComObject "MOM.ScriptAPI"
 $PropertyBag = $ScomAPI.CreatePropertyBag()
-    
+
 # Import the PowerShell Active Directory Module
 Import-Module ActiveDirectory
-            
+
     try {
         #Get group members
         $Members = Get-ADGroupMember -Identity $ADGroup
-        
+
         #Prepare a hash table
         $Accounts = @{}
 
         if ($Members) {
-        
-            foreach ($Member in $Members){        
+
+            foreach ($Member in $Members){
                 #Get the AccountExiration Date for each user
                 $AccountExpiration = Get-ADUser -Filter {Name -eq $Member.Name} –Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" | Select-Object -Property "Displayname",@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}}
                 $AccountExpirationDate = $AccountExpiration.ExpiryDate
@@ -63,41 +63,45 @@ Import-Module ActiveDirectory
                 [array]$WarnUsers = @()
 
                 foreach ($hash in $Accounts.GetEnumerator()) {
-            
+
                 #Build the variable to work with
                 [string]$UserName = $hash.Key
                 [datetime]$PwdExpDate = $hash.Value
 
                     #Account is either expired or is about to expire in the next 20 days
                     if (($PwdExpDate -gt $CurrentDate) -and ($PwdExpDate -lt $WarningDate)) {
-                    
+
                     #Add the user to the array of Users for which an error will be raised
-                    $WarnUsers += $UserName                   
-                    }                             
+                    $WarnUsers += $UserName
+                    }
                 }
-                
+
                 #Build User Count value
                 [int]$WarnUserCount = $WarnUsers.Count
-                            
-                if  ($WarnUserCount -gt 0) { 
-                
+
+                if  ($WarnUserCount -gt 0) {
+
                     [string]$Users = $WarnUsers | Out-String
 
-                    #Create and fill the case specific Property Bags               
+                    #Create and fill the case specific Property Bags
                     $PropertyBag.AddValue("WarnUserCount","$WarnUserCount")
-                    $PropertyBag.AddValue("State","Warning")                
-                    $PropertyBag.AddValue("ExpiredUsers",$Users)                            
-                
-                } 
-                                           
-            } else {            
+                    $PropertyBag.AddValue("State","Warning")
+                    $PropertyBag.AddValue("ExpiredUsers",$Users)
+
+                } else {
+                #No Account are expiring. Create a Property Bag with a zero count
+                $PropertyBag.AddValue("State", "NoPasswordExpirations")
+
+                }
+
+            } else {
             #No Account are expiring. Create a Property Bag with a zero count
             $PropertyBag.AddValue("State","NoPasswordExpirations")
           }
-        } 
+        }
 
-    } finally {        
-      # Send the whole output to SCOM        
+    } finally {
+      # Send the whole output to SCOM
       $PropertyBag
       #Used for testing
       #$ScomAPI.Return($PropertyBag)
